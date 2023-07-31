b5869b65c4d3d10b15a2ba27c5d257e974437c0f
nithiyananthamQA <nathianand10@gmail.com>
2023-07-31 18:07:17 +0530

    test

--- tests/Icliniq/Pages/LoginPage.ts
+++ tests/Icliniq/Pages/LoginPage.ts
@@ -1,8 +1,9 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 let page:Page
 export default class Login{
 
  private page:Page
+ //page
 
     constructor (page:Page) {
         this.page = page;
@@ -11,6 +12,7 @@ export default class Login{
     public async UsernameInput(){
         const UsrinputBox = await this.page.locator('[id="username"]')
         await UsrinputBox.fill('2375')
+    
          if(UsrinputBox != null){
              return UsrinputBox
          }
@@ -20,9 +22,9 @@ export default class Login{
     public async PasswordInput(){
 
         const PwdInput = await this.page.locator('#rpassword').fill('@Ni12345')
-        // if(PwdInput != null){
-        //     return PwdInput
-        // }
+        if(PwdInput != null){
+            return PwdInput
+        }
     }
 
     public async LoginButton(){
